de42af8edfbd56348cbd0d37fe19da479b82757b
Vishnu <gundpa45@gmail.com>
2025-07-28 01:51:36 +0530

    some changes are made

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,56 @@
 
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
  
 
 function App() {
 
-  const [message,setMessage]=useState(["hello welcome to my chat app " ])
+  const [message,setMessage]=useState(["hei there ", "hello" ])
+
+
+    const wsRef = useRef();
   useEffect(()=>{
-     const ws = new WebSocket("http://localhost:5173/")
+     const ws = new WebSocket("http://localhost:8080/")
      ws.onmessage=(event)=>{
      setMessage(m=>[...m, event.data])
+     }
+     wsRef.current=ws;
+     ws.onopen=()=>{
+      ws.send(JSON.stringify({
+        type:"join",
+        payload:{
+          roomId:"red"
+        }
+      }))
      }
   },[])
 
   return (
-    <div className='bg-black text-white h-screeb '>
-      <div className="h-[90vh] bg-red-300">{message.map(message=>{
-        <div>{message}</div>
-      })}</div>
-      <div className=' bg-white text-black flex '>
-        <input type="text" placeholder='type here' className='flex-1  ' />
-        <button className='bg-purple-600  p-4 '>Send message</button>
+    <div className='bg-black text-white h-screen '>
+      <br /><br /><br /> 
+      <div className="h-[85vh] ">
+        {message.map(message=>
+         <div className='m-8 '>
+          <span className='bg-white text-black rounded p-4  '>
+          {message}
+          </span>
+         </div>
+      )}</div>
+      <div className='w-full bg-white text-black flex '>
+        <input type="text" placeholder='type here' className='flex-1 p-4 ' />
+        <button
+        onClick={()=>{
+          wsRef.current.send(JSON.stringify({
+            type:"chat",
+            payload:{
+              message:message
+            }
+          }))
+        }}
+        className='bg-purple-600  p-4 '>
+          Send message
+          </button>
       </div>
     </div>
   )
